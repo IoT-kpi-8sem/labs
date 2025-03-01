bbc213e9454bcae31fe54497dd93332671f40f2f
KosieVich <junkjuk5645@gmail.com>
2025-03-02 01:55:35 +0200

    Add sending to hub

--- Edge/Edge/Edge/Adapters/HubAdapter.cs
+++ Edge/Edge/Edge/Adapters/HubAdapter.cs
@@ -1,12 +1,18 @@
-﻿using System.Text.Json;
+﻿using System.Net.Http.Json;
+using System.Text.Json;
 using Edge.Entities;
 
 namespace Edge.Adapters;
 
-public class HubAdapter(string host, int port)
+public class HubAdapter(string endpoint)
 {
+    private readonly HttpClient _client = new();
+
     public async Task SendData(ProcessedAgentData data)
     {
         Console.WriteLine(data.RoadState);
+        var content = new StringContent(JsonSerializer.Serialize(data));
+        content.Headers.ContentType = new("application/json");
+        await _client.PostAsync(endpoint, content);
     }
 }
--- Edge/Edge/Edge/Program.cs
+++ Edge/Edge/Edge/Program.cs
@@ -3,7 +3,7 @@ using Edge.Adapters;
 
 var mqttClient = new MyMqttClient("127.0.0.1", 1883, "clientId2");
 var agent = new AgentAdapter(mqttClient);
-var hub = new HubAdapter("123", 312);
+var hub = new HubAdapter("http://localhost:5062/roadData");
 var processor = new DataProcessor(agent, hub);
 await processor.Start();
 while (true)
